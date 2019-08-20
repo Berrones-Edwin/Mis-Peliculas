f2e0a3166555162e29aace1ee6eec150b7e5d29c
Berrones Edwin <Berrones.Edwin@hotmail.com>
2019-08-20 18:44:11 -0500

    Se empezo el componente clasificacion

---
+++ src/app/Data/classifications.ts
@@ -0,0 +1,24 @@
+import { Classification } from '../interfaces/classifications';
+
+export const classifications: Classification[] = [
+    {
+      name: 'Clasificación G',
+      text: 'Todas las edades / Todos los publicos'
+    },
+    {
+      name: 'Clasificación PG',
+      text: '+10años / Algun material puede ser no adecuado para niños menores de 10'
+    },
+    {
+      name: 'Clasificación PG-13',
+      text: '+13 / Algunos materiales puede ser no adecuado para niños menores de 13'
+    },
+    {
+      name: 'Clasificación R',
+      text: '+17 / Menores de 17 años requieren acompañamiento de pares o tutor adulto. Puede contener lenguaje fuerte, intensa enfasis sexual, desnudez explicita y fuerte,fuerte contenido de violencia, uso muy frecuente de arma y sangre'
+    },
+    {
+      name: 'Clasificación NC-17',
+      text: '+18 Pelicula no es adecuada los menores de 18 años. Casi igual que la clasificacion R pero lo unico que cambia es que ahui hay mucha violencia extrema, contenido sexuales fuerte, extremo uso de armsas, desnudez en largos periodos tiempo y/o mucha sangre'
+    },
+  ]
