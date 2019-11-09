fa0755f9f655be61693a44b7e3731bf1a44d9215
Berrones Edwin <Berrones.Edwin@hotmail.com>
2019-11-09 15:29:59 -0600

    se empezo la autenticacion

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-navbar',
@@ -9,24 +11,68 @@ import { AuthService } from 'src/app/services/auth.service';
 export class NavbarComponent implements OnInit {
 
   constructor(
-    private _authService: AuthService
+    private _authService: AuthService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
   ) {
   }
 
-  token: {};
+  request_token: {};
+  request_token_approved: string;
+  session_id: string;
+
   ngOnInit() {
+    // if(this.request_token)
+    //   this.getTokenApproved();
+
+    if (window.location.href.split('?')[1] !== undefined) {
+      this.request_token_approved = window.location.href.split('?')[1].split('&')[0].split('=')[1];
+      console.log(this.request_token_approved)
+
+      if (this.request_token_approved.length > 0)
+        this.createNewSession(this.request_token_approved)
+          .then((data) => {
+            this._authService.session_id = data['session_id'];
+            console.log(this._authService.session_id)
+            console.log(data['session_id'])
+          })
+    }
+  }
+
+  getTokenApproved() {
+
+    console.log('aqui')
+    // if(window.location.href.split('?')[1] === undefined || window.location.href.split('?')[1] === null)
+    //   return;
+
+    this.request_token_approved = window.location.href.split('?')[1].split('&')[0].split('=')[1];
+    console.log(this.request_token_approved)
+
+    if (this._authService.session_id.length > 0)
+      this.createNewSession(this.request_token_approved)
+        .then((data) => {
+          this._authService.session_id = data['session_id'];
+          console.log(this._authService.session_id)
+        })
+
   }
 
   login() {
     this.getNewToken()
       .then((data) => {
-        this.token = data;
-        return this.getPermissionUser(this.token['request_token'])
+        this.request_token = data;
+        console.log(this.request_token);
+
+        return this.getPermissionUser(this.request_token['request_token'])
       })
       .then((data) => {
         console.log(data)
       })
 
+  }
+
+  logout(){
+    
   }
 
   getNewToken(): Promise<any[]> {
@@ -39,9 +85,13 @@ export class NavbarComponent implements OnInit {
     })
   }
 
-  getPermissionUser(token: string): Promise<any[]> {
+
+  getPermissionUser(token: string) {
+    this._authService.getPermissionUser(token);
+  }
+  createNewSession(requestToken: string): Promise<any[]> {
     return new Promise((resolve, reject) => {
-      this._authService.getPermissionUser(token).subscribe((data: any) => {
+      this._authService.createNewSession(requestToken).subscribe((data: any) => {
         if (data) resolve(data)
         else reject()
       },
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -8,6 +8,7 @@ import { environment } from 'src/environments/environment';
 export class AuthService {
 
   private apiKey = '1de6ce733dd02d81073262cb66031536';
+  public session_id:string = '';
 
   constructor(
     private _http: HttpClient
@@ -24,7 +25,7 @@ export class AuthService {
   }
 
   getPermissionUser(token: string) {
-    return this._http.get(`https://www.themoviedb.org/authenticate/${token}?redirect_to=http://localhost:4200/inicio`);
+    window.location.href = `https://www.themoviedb.org/authenticate/${token}?redirect_to=http://localhost:4200/inicio`;
   }
 
   createNewSession(request_token: string) {
@@ -34,6 +35,23 @@ export class AuthService {
       .append('language', 'es');
 
     return this._http.post(`${environment.url}/authentication/session/new`, { "request_token": request_token }, { params });
+
+  }
+  logout(session_id: string) {
+
+    // let params: HttpParams = new HttpParams()
+    //   .append('api_key', this.apiKey)
+
+      const options = {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json'
+        }),
+        body: {
+          "session_id" : session_id
+        }
+      }
+
+    return this._http.delete(`${environment.url}/authentication/session/new?api_key=${this.apiKey}`,options);
 
   }
 
