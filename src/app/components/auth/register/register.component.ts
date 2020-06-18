9672fccf82d2cdd3711c6f914fe5e3e8533fb160
Berrones Edwin <Berrones.Edwin@hotmail.com>
2020-06-17 20:57:05 -0500

    se realizo el diseño del registro de usuario

--- src/app/components/auth/register/register.component.ts
+++ src/app/components/auth/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
+import { responseLogin } from 'src/app/interfaces/ResponseLogin.interface';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,70 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  public form: FormGroup;
+
+  constructor(
+    private formBuilder:FormBuilder,
+    private _authService: AuthService
+  ) { }
 
   ngOnInit(): void {
+    this.createForm();
   }
+
+  login(form: FormGroup) {
+
+    const { email, password } = form.value;
+
+    this._authService.login(email, password).subscribe(console.log);
+  }
+
+  createForm() {
+    this.form = this.formBuilder.group({
+      name: ["", Validators.required],
+      email: ["", Validators.required],
+      password: ["", [Validators.required, Validators.minLength(8)]],
+      repeat_password: ["", [Validators.required, Validators.minLength(8)]],
+    })
+  }
+
+  get name() {
+    return this.form.get('name');
+  }
+  get nameIsValid() {
+    return this.name.valid && this.name.touched;
+  }
+  get nameIsInvalid() {
+    return this.name.invalid && this.name.touched;
+  }
+  get email() {
+    return this.form.get('email');
+  }
+  get emailIsValid() {
+    return this.email.valid && this.email.touched;
+  }
+  get emailIsInvalid() {
+    return this.email.invalid && this.email.touched;
+  }
+  get password() {
+    return this.form.get('password');
+  }
+  get passwordIsValid() {
+    return this.password.valid && this.password.touched;
+  }
+  get passwordIsInvalid() {
+    return this.password.invalid && this.password.touched;
+  }
+  get repeat_password() {
+    return this.form.get('repeat_password');
+  }
+  get repeat_passwordIsValid() {
+    return this.repeat_password.valid && this.repeat_password.touched;
+  }
+  get repeat_passwordIsInvalid() {
+    return this.repeat_password.invalid && this.repeat_password.touched;
+  }
+
+
 
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,18 +2,19 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { userResponseLogin } from '../interfaces/ResponseLogin.interface';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-
   public token: string = '';
   public user: userResponseLogin;
 
   constructor(
-    private _http: HttpClient
+    private _http: HttpClient,
+    private _router: Router
   ) {
 
     if (localStorage.getItem('token') !== null || localStorage.getItem('token') !== undefined) {
@@ -31,6 +32,13 @@ export class AuthService {
     params.append('password', password)
 
     return this._http.post(`${environment.urlApi}auth/login`, { 'email': email, 'password': password }, { headers });
+  }
+
+  logout() {
+    this.token = "";
+    this.user = null;
+    localStorage.clear();
+    this._router.navigate(['/']);
   }
 
 }
